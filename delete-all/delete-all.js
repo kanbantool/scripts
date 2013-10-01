85132dd5070c25ff9c502aeee6c7b527acc3c922
Mateusz Ksiazek <m.ksiazek@shorelabs.com>
2013-10-01 10:33:51 +0200

    Some fixes and added better descriptions about 'delete all' script.

--- delete-all/delete-all.js
+++ delete-all/delete-all.js
@@ -3,14 +3,14 @@
 
   css = " .contextMenu li.delete_all a { background-image: url(/images/icons/silk/note_delete.png);} ";
 
-  if (board.model.users.get(KanbanTool.Board.current_user_id).can('delete_tasks', board.model)) {
+  if (window.board.model.users.get(KanbanTool.Board.current_user_id).can('delete_tasks', window.board.model)) {
     $('<style>').html(css).appendTo('head');
     $('#cell_context_menu').prepend('<li class="delete_all"><a href="#extension:delete_all">Delete all</a></li>');
     $(window).bind('extensionContextMenuAction', function(event, action, el, pos) {
       var cell, tasks_collection;
       if (action === 'extension:delete_all') {
         cell = $(el)[0].id.split('_');
-        tasks_collection = board.model.tasks.where({
+        tasks_collection = window.board.model.tasks.where({
           "workflow_stage_id": parseInt(cell[2]),
           "swimlane_id": parseInt(cell[4])
         });
