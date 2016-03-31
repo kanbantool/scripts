d4d84ceff2b4012be7a3e712baefc1375b2b43cf
Zbyszek Zemla <zbyszek@shorelabs.com>
2016-03-31 11:47:43 +0200

    [save-done] Fix issue with timer not being stopped after task is marked as done

--- save-done/save-done.js
+++ save-done/save-done.js
@@ -1,10 +1,10 @@
 KT.Elements.Taskview.footer.push({
   __: 'save-done',
-  filter: function(el,task) {
+  filter: function(el, task) {
     var ref;
     return task.id && ((ref = task.workflowStage()) != null ? ref.next() : void 0);
   },
   html: function() {
-    return "<button class=\"btn btn-small\" onclick=\"var task = $(event.target).addClass('disabled').closest('kt-taskview')[0].kt.task; task.save({'workflow_stage_id':task.board().workflowStages().leafs().pop().id, 'position':null}).done(function(){KT.Elements.Taskview.close();}); return false;\">Move to done</button>";
+    return "<button class=\"btn btn-small\" onclick=\"var task = $(event.target).addClass('disabled').closest('kt-taskview')[0].kt.task; KT.tasks.groupUpdate([task], {'workflow_stage_id':task.board().workflowStages().leafs().pop().id, 'position':null}).done(function(){KT.Elements.Taskview.close();}); return false;\">Move to done</button>";
   }
 });
--- save-done/src/save-done.coffee
+++ save-done/src/save-done.coffee
@@ -1,5 +1,5 @@
 KT.Elements.Taskview.footer.push({
   __: 'save-done'
   filter: (el, task) -> task.id && task.workflowStage()?.next()
-  html: -> """<button class="btn btn-small" onclick="var task = $(event.target).addClass('disabled').closest('kt-taskview')[0].kt.task; task.save({'workflow_stage_id':task.board().workflowStages().leafs().pop().id, 'position':null}).done(function(){KT.Elements.Taskview.close();}); return false;">Move to done</button>"""
+  html: -> """<button class="btn btn-small" onclick="var task = $(event.target).addClass('disabled').closest('kt-taskview')[0].kt.task; KT.tasks.groupUpdate([task], {'workflow_stage_id':task.board().workflowStages().leafs().pop().id, 'position':null}).done(function(){KT.Elements.Taskview.close();}); return false;">Move to done</button>"""
 })
