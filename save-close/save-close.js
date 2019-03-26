58dcdb32f4856dff9795e2853fbdfaeb48d6936b
Bjoern <bjohas@users.noreply.github.com>
2019-03-26 17:04:26 +0000

    Update save-close.js
    Changed
    > return task.id && ((ref = task.workflowStage()) != null ? ref.next() : void 0)
    to 
    > return true;
    i.e. the close button now also appears in the 'done' column.

--- save-close/save-close.js
+++ save-close/save-close.js
@@ -2,7 +2,7 @@ KT.Elements.Taskview.footer.push({
   __: 'save-close',
   filter: function(el, task) {
     var ref;                                                                                                                                                                      
-    return task.id && ((ref = task.workflowStage()) != null ? ref.next() : void 0);                                                                                               
+    return true;                                                                                               
   },
   html: function() {
     return "<button class=\"btn btn-small\" onclick=\"KT.Elements.Taskview.close(); return false;\">Close</button>";                                                              
