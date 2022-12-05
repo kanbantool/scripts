804fe3a42eec9466c662eb8cf7a1285791ee15c1
Ali Soleimani <30648015+alisolphp@users.noreply.github.com>
2022-12-05 18:48:22 +0330

    [fix] checklist-progressbar.js
    Displays a more nice progress bar just on page load.

--- checklist-progressbar/checklist-progressbar.js
+++ checklist-progressbar/checklist-progressbar.js
@@ -1,20 +1,28 @@
 function makebar(kde) {
-  title=$(kde).find(".checklist-indicator").attr("title");
+    title = $(kde).find(".checklist-indicator").attr("title");
 
-  if (title!==undefined) {
-    n=title.indexOf("%");
-    prc=title.substr(0,n);    
-    $('kt-task').unbind('DOMSubtreeModified');
-    $(kde).find(".kt-task-footer").find("hr").remove();
-    $(kde).find(".kt-task-footer").prepend("<hr style=\"width:"+prc+"%; height:5px; background:#fff;\">");            
-    $('kt-task').bind('DOMSubtreeModified', function(){ makebar(this); });
-  } 
+    if (title !== undefined) {
+        prc = title.split("%")[0];
+        $('kt-task').unbind('DOMSubtreeModified');
+        $(kde).find(".kt-task-footer").find("hr").remove();
+
+        if ($(kde).find(".kt-task-footer > span").length == false) {
+            $(kde).find(".kt-task-footer").prepend("<span style=\"display:block;width:100%;background: #fff;border-radius:5px;\"><span style=\"display:block;width:" + prc + "%;  background:#1fb5d7;text-align:center;\">" + prc + "%</span></span>");
+        }
+        $('kt-task').bind('DOMSubtreeModified', function() {
+            makebar(this);
+        });
+    }
 }
 
-$(window).on('kt-task:render', function(e) {
-  makebar(e.target);
+$('kt-task').bind('DOMSubtreeModified', function() {
+    makebar(this);
 });
 
-$('kt-task').bind('DOMSubtreeModified', function(){
-  makebar(this);
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".checklist-indicator").each(function(index) {
+            makebar($(this).parent().parent());
+        });
+    }, 100);
 });
