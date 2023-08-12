d3d63f2b4ef8c72bde37fdb71236b9d3f8a84fe6
Sylwia Poterala <sylwia.poterala@gmail.com>
2023-08-12 07:53:53 +0200

    checklist progressbar optimization

--- checklist-progressbar/checklist-progressbar.js
+++ checklist-progressbar/checklist-progressbar.js
@@ -4,10 +4,8 @@ function makebar(kde) {
   if (title!==undefined) {
     n=title.indexOf("%");
     prc=title.substr(0,n);    
-    $('kt-task').unbind('DOMSubtreeModified');
     $(kde).find(".kt-task-footer").find("hr").remove();
     $(kde).find(".kt-task-footer").prepend("<hr style=\"width:"+prc+"%; height:5px; background:#fff;\">");            
-    $('kt-task').bind('DOMSubtreeModified', function(){ makebar(this); });
   } 
 }
 
@@ -15,6 +13,10 @@ $(window).on('kt-task:render', function(e) {
   makebar(e.target);
 });
 
-$('kt-task').bind('DOMSubtreeModified', function(){
-  makebar(this);
+KT.onInit(function() {
+  return setTimeout(function() {
+    return $('kt-task').each(function() {
+      return makebar(this);
+    });
+  }, 750);
 });
--- checklist-progressbar/src/checklist-progessbar.coffee
+++ checklist-progressbar/src/checklist-progessbar.coffee
@@ -3,14 +3,13 @@ makebar = (kde) ->
   if title != undefined
     n = title.indexOf('%')
     prc = title.substr(0, n)
-    $('kt-task').unbind 'DOMSubtreeModified'
     $(kde).find('.kt-task-footer').find('hr').remove()
     $(kde).find('.kt-task-footer').prepend '<hr style="width:' + prc + '%; height:5px; background:#fff;">'
-    $('kt-task').bind 'DOMSubtreeModified', ->
-      makebar this
 
 $('kt-task').each ->
   makebar(this)
 
-$('kt-task').bind 'DOMSubtreeModified', ->
-  makebar(this)
+KT.onInit ->
+  setTimeout ->
+    $('kt-task').each -> makebar(@)
+  , 750
